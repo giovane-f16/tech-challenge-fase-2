9d0bf0cbfb82627ef6f97a0e35ca2d866121a12a
giovanef16 <giovane_05silva@hotmail.com>
2025-07-24 08:39:06 -0300

    adicionando lib e resolvendo warning

--- app/index.ts
+++ app/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import createRouter from "./src/routes/Posts";
-import swaggerUi from "swagger-ui-express";
+import swaggerUi, { JsonObject } from "swagger-ui-express";
 import swaggerDocument from "./src/config/swagger";
 
 const app = express();
@@ -13,7 +13,7 @@ async function init() {
     const router = await createRouter();
     const porta = process.env.PORT || 3000;
     app.use("/posts", router);
-    app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+    app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument as JsonObject));
     app.listen(porta, () => {
         console.log(`Servidor rodando em http://localhost:${porta}`);
     });
